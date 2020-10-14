284b8c554ac788a3d5b9b268fe1f587f4c1d1e4a
Viacheslav Akulov <sentence@ngs.ru>
2020-10-15 00:23:48 +0700

    Разбивает верстку на компоненты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Header from './components/Header/header.js';
+import Main from './components/Main/main.js';
+import Features from './components/Features/features.js';
+import Calendar from './components/Calendar/calendar.js';
+import Details from './components/Details/details.js';
+import Footer from './components/Footer/footer.js';
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Features />
+      <Footer />
+    </>
   );
 }
 
